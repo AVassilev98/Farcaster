b244245252840668c0e10758ef86414a68370b64
Anton Vassilev <anton@Mac.attlocal.net>
2026-03-28 04:02:45 -0500

    Use vertex buffer for rendering

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 
+#include "cglm/types.h"
 #include "photon/photon_device.h"
 #include "photon/photon_log.h"
+#include "photon/photon_mesh.h"
 #include "photon/photon_pipeline.h"
 #include "photon/photon_status.h"
 #define GLFW_INCLUDE_VULKAN
@@ -9,7 +11,7 @@
 
 #include "stdio.h"
 
-PhStatus renderTriangle(PhDeviceHandle device, PhPipeline *pipeline, PhImage *image, PhSemaphore *pWait, PhSemaphore *pSignal)
+PhStatus renderTriangle(PhDeviceHandle device, PhPipeline *pipeline, PhMesh *mesh, PhImage *image, PhSemaphore *pWait, PhSemaphore *pSignal)
 {
     PhCommandBuffer buffer = { 0 };
     uint32_t imageIndex = 0;
@@ -20,7 +22,7 @@ PhStatus renderTriangle(PhDeviceHandle device, PhPipeline *pipeline, PhImage *im
         .flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT,
     };
     PH_CHECK(PH_LOG_ERROR, ph_device_semaphore_create(device, &finishedSemaphore));
-    PH_CHECK(PH_LOG_ERROR, ph_device_command_buffer_create(device, PH_QUEUE_TYPE_GRAPHICS, 1, &buffer));
+    PH_CHECK(PH_LOG_ERROR, ph_device_command_buffer_create(device, PH_QUEUE_TYPE_GRAPHICS_BIT, 1, &buffer));
     PH_VK_CHECK(PH_LOG_ERROR, vkBeginCommandBuffer(buffer, &beginInfo));
 
 
@@ -78,6 +80,8 @@ PhStatus renderTriangle(PhDeviceHandle device, PhPipeline *pipeline, PhImage *im
     VkRect2D scissor = { .offset = { 0, 0 }, .extent = image->extent };
     vkCmdSetScissor(buffer, 0, 1, &scissor);
 
+    VkDeviceSize offset = 0;
+    vkCmdBindVertexBuffers(buffer, 0, 1, &mesh->gpuVertexBuffer.buffer, &offset);
     vkCmdDraw(buffer, 3, 1, 0, 0);
 
     vkCmdEndRendering(buffer);
@@ -99,7 +103,7 @@ PhStatus renderTriangle(PhDeviceHandle device, PhPipeline *pipeline, PhImage *im
     };
 
     PH_CHECK(PH_LOG_ERROR, 
-        ph_device_queue_submit(device, PH_QUEUE_TYPE_GRAPHICS, &submitInfo));
+        ph_device_queue_submit(device, PH_QUEUE_TYPE_GRAPHICS_BIT, &submitInfo));
     
     *pSignal = finishedSemaphore;
     return PH_SUCCESS;
@@ -117,6 +121,7 @@ int main(void) {
     PhGraphicsPipelineOptions pipelineOptions   = PH_PIPELINE_OPTIONS_DEFAULT;
     PhPipeline pipeline                         = { 0 };
     PhImage presentImage                        = { 0 };
+    PhMesh  mesh                                = { 0 };
 
     PhInstanceSettings instanceSettings = {
         .appName = "Farcaster",
@@ -153,7 +158,7 @@ int main(void) {
         .dynamicRendering = true,
     };
 
-    PH_CHECK(PH_LOG_ERROR, ph_enumerate_devices(hInstance, deviceCaps, &deviceInfos));
+    PH_CHECK(PH_LOG_ERROR, ph_devices_enumerate(hInstance, deviceCaps, &deviceInfos));
 
     presentOptions = (PhPresentOptions) {
         .format = {
@@ -164,11 +169,53 @@ int main(void) {
     };
     PH_CHECK(PH_LOG_ERROR, ph_window_get_surface(hWindow, &hSurface));
     chosenDevice = deviceInfos.ptr[0].handle;
-    PH_CHECK(PH_LOG_ERROR, ph_configure_device_for_present(chosenDevice, hSurface, presentOptions));
+    PH_CHECK(PH_LOG_ERROR, ph_device_configure_for_present(chosenDevice, hSurface, presentOptions));
     PH_CHECK(PH_LOG_ERROR, ph_create_shader_module(deviceInfos.ptr[0].handle, SHADER_DIR "/triangle.spv", &triangleShader));
+
+    static PhVertex vertices[] = {
+        {
+            {0, -0.5},
+            {1.0, 0.0, 0.0},
+        },
+        {
+            {0.5, 0.5},
+            {0.0, 1.0, 0.0},
+        },
+        {
+            {-0.5, 0.5},
+            {0.0, 0.0, 1.0},
+        },
+    };
+
 
     VkFormat colorFormat = VK_FORMAT_B8G8R8A8_SRGB;
     PhColorBlendAttachmentOptions blendAttachment = PH_COLOR_BLEND_ATTACHMENT_OPTIONS_DEFAULT;
+
+    VkVertexInputAttributeDescription vertexAttributes[] = {
+        {
+            0,
+            0,
+            VK_FORMAT_R32G32_SFLOAT,
+            offsetof(PhVertex, position)
+        },
+        {
+            1,
+            0,
+            VK_FORMAT_R32G32B32_SFLOAT,
+            offsetof(PhVertex, color)
+        },
+    };
+
+    VkVertexInputBindingDescription vertexBinding = {
+        .binding = 0,
+        .inputRate = VK_VERTEX_INPUT_RATE_VERTEX,
+        .stride = sizeof(PhVertex),
+    };
+
+    pipelineOptions.inputStateInfo.pVertexAttributeDescriptions = vertexAttributes;
+    pipelineOptions.inputStateInfo.vertexAttributeDescriptionCount = 2;
+    pipelineOptions.inputStateInfo.pVertexBindingDescriptions = &vertexBinding;
+    pipelineOptions.inputStateInfo.vertexBindingDescriptionCount = 1;
 
     pipelineOptions.pShaders                = &triangleShader;
     pipelineOptions.shaderCount             = 1UL;
@@ -176,12 +223,14 @@ int main(void) {
     pipelineOptions.colorAttachmentCount    = 1;
     pipelineOptions.pColorBlendAttachments  = &blendAttachment;
     PH_CHECK(PH_LOG_ERROR, ph_create_graphics_pipeline(chosenDevice, pipelineOptions, &pipeline));
+    PH_CHECK(PH_LOG_ERROR, ph_device_create_staging_buffer(chosenDevice, 1024*1024*64));
+    PH_CHECK(PH_LOG_ERROR, ph_mesh_create(chosenDevice, vertices, PH_NUM_ELEMS(vertices), &mesh));
 
     while(!ph_window_should_close(hWindow))
     {
         PhSemaphore renderSemaphore;
         PH_CHECK(PH_LOG_ERROR, ph_device_present_image_get_next(chosenDevice, &presentImage));
-        PH_CHECK(PH_LOG_ERROR, renderTriangle(chosenDevice, &pipeline, &presentImage, &presentImage.readySemaphore, &renderSemaphore));
+        PH_CHECK(PH_LOG_ERROR, renderTriangle(chosenDevice, &pipeline, &mesh, &presentImage, &presentImage.readySemaphore, &renderSemaphore));
         PH_CHECK(PH_LOG_ERROR, ph_device_present(chosenDevice, &renderSemaphore, 1UL));
         ph_window_poll_events(hWindow);
     }
